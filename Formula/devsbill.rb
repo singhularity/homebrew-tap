5dd196e817d9c170401ce7fea9e8a53f7d4147dc
Saurabh Singh <saurabhsin@gmail.com>
2026-03-30 00:19:41 -0400

    fix: update R2 public URL

--- Formula/devsbill.rb
+++ Formula/devsbill.rb
@@ -6,20 +6,20 @@ class Devsbill < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://pub-508609135146446e8f26cb60c3330e02.r2.dev/devsbill/v0.0.0/devsbill-v0.0.0-darwin-arm64.tar.gz"
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/devsbill/v0.0.0/devsbill-v0.0.0-darwin-arm64.tar.gz"
       sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     else
-      url "https://pub-508609135146446e8f26cb60c3330e02.r2.dev/devsbill/v0.0.0/devsbill-v0.0.0-darwin-amd64.tar.gz"
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/devsbill/v0.0.0/devsbill-v0.0.0-darwin-amd64.tar.gz"
       sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://pub-508609135146446e8f26cb60c3330e02.r2.dev/devsbill/v0.0.0/devsbill-v0.0.0-linux-arm64.tar.gz"
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/devsbill/v0.0.0/devsbill-v0.0.0-linux-arm64.tar.gz"
       sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     else
-      url "https://pub-508609135146446e8f26cb60c3330e02.r2.dev/devsbill/v0.0.0/devsbill-v0.0.0-linux-amd64.tar.gz"
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/devsbill/v0.0.0/devsbill-v0.0.0-linux-amd64.tar.gz"
       sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     end
   end
