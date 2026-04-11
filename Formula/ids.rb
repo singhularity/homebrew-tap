c87f869bdfd4c9b8d4a5c29bc003293d0252ab9f
Saurabh Singh <saurabhsin@gmail.com>
2026-04-11 15:38:43 -0400

    ids 0.4.2

--- Formula/ids.rb
+++ Formula/ids.rb
@@ -1,16 +1,16 @@
 class Ids < Formula
   desc "Portfolio operations platform for serial builders"
   homepage "https://indieprise.com"
-  version "0.4.1"
+  version "0.4.2"
   license "Proprietary"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/ids/v0.4.1/ids-v0.4.1-darwin-arm64.tar.gz"
-      sha256 "471224770407b8084decaca89201b2a2a0254b50045093e0f741ab593a044568"
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/ids/v0.4.2/ids-v0.4.2-darwin-arm64.tar.gz"
+      sha256 "7c43a8e08f027a550fa2e5416f3ef3516168c88cc2a5068fe2a982f7fcb835ae"
     else
-      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/ids/v0.4.1/ids-v0.4.1-darwin-amd64.tar.gz"
-      sha256 "92efaf8f1cf1062f057a699e33728ca1c78011b17e45a271130689500fd75da7"
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/ids/v0.4.2/ids-v0.4.2-darwin-amd64.tar.gz"
+      sha256 "313ea9cee810ff8d13b3c1ca363682603f89ecaeccf002a8761492b57516fa81"
     end
   end
 
