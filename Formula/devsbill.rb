377a9ccb49425736f9eed335f006636e8915c320
Saurabh Singh <saurabhsin@gmail.com>
2026-06-18 09:38:13 -0400

    devsbill v0.5.1 (devlab auto-bump)

--- Formula/devsbill.rb
+++ Formula/devsbill.rb
@@ -1,26 +1,26 @@
 class Devsbill < Formula
   desc "Multi-cloud cost tracker with IaC scanning"
-  homepage "https://indieprise.com"
-  version "0.5.0"
+  homepage "https://devsbill.com"
+  version "0.5.1"
   license "Proprietary"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/devsbill/v0.5.0/devsbill-v0.5.0-darwin-arm64.tar.gz"
-      sha256 "8e1253b949f39c72556a75af9d1ebe41ea218641e91b23d47b703d3b5dfbfa05"
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/devsbill/v0.5.1/devsbill-v0.5.1-darwin-arm64.tar.gz"
+      sha256 "f9b8bdd3da4a5064fcb70e067b0f0aadf006d121b44ed447c55853b15a38d03a"
     else
-      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/devsbill/v0.5.0/devsbill-v0.5.0-darwin-x64.tar.gz"
-      sha256 "a7f06c841aee1d31ad3649300372165f4ba4ee184f1bff937e439631089ed8d8"
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/devsbill/v0.5.1/devsbill-v0.5.1-darwin-x64.tar.gz"
+      sha256 "9f32a7b2e93319a6009dbccb4ab909004e5ca0d64337b1e8eb96e40374438495"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/devsbill/v0.5.0/devsbill-v0.5.0-linux-arm64.tar.gz"
-      sha256 "4a9b2024c9e4f0e1abedce9d409309c468123642e3cd608ee661a0134a621e46"
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/devsbill/v0.5.1/devsbill-v0.5.1-linux-arm64.tar.gz"
+      sha256 "47f02a05953bb43b93c0000af7f75d826304684375726490a60089de98528be0"
     else
-      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/devsbill/v0.5.0/devsbill-v0.5.0-linux-amd64.tar.gz"
-      sha256 "d8aa0f68efecb396c29f801d5dcfafc8da918cfbbe6093233a00c77315341c28"
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/devsbill/v0.5.1/devsbill-v0.5.1-linux-amd64.tar.gz"
+      sha256 "3bb564b1407e0623570c6af3da2c3889ec54bcb2f3e186a52991b39596ca7551"
     end
   end
 
