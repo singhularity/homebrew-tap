077d92191c4d432afc7b4e1d5f3dede222c82275
Saurabh Singh <saurabhsin@gmail.com>
2026-06-18 21:27:40 -0400

    migrataur v0.8.3 (devlab auto-bump)

--- Formula/migrataur.rb
+++ Formula/migrataur.rb
@@ -1,23 +1,26 @@
 class Migrataur < Formula
   desc "3-tier code migration agent"
   homepage "https://migrataur.com"
-  version "0.8.2"
+  version "0.8.3"
   license "Proprietary"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/migrataur/v0.8.2/migrataur-v0.8.2-darwin-arm64.tar.gz"
-      sha256 "d83097167cd4bcd193e0674e17473d773433ea3e80255c3a94a7121ebeb92bd2"
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/migrataur/v0.8.3/migrataur-v0.8.3-darwin-arm64.tar.gz"
+      sha256 "a71e03a98d0464c459686d97318aa01b75cdee2e89592010fa759bedfe639ad4"
     else
-      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/migrataur/v0.8.2/migrataur-v0.8.2-darwin-x64.tar.gz"
-      sha256 "faba28ec31b4a3a940c02509f1333656a03c912651afafe44eb4851189fe052e"
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/migrataur/v0.8.3/migrataur-v0.8.3-darwin-x64.tar.gz"
+      sha256 "06e1e2185c6e6ee0b3bbc12f4174bc8a3fc92ee1892325038453525150f2e890"
     end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/migrataur/v0.8.2/migrataur-v0.8.2-linux-amd64.tar.gz"
-      sha256 "49e99cd0fe26be255949e21a060a2b8410aedcec5380081ee6a1d1c5b03cd80a"
+    if Hardware::CPU.arm?
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/migrataur/v0.8.3/migrataur-v0.8.3-linux-arm64.tar.gz"
+      sha256 "8099521dbc75f0bd2f08f9277e6d0e7f2ae40ebf9b92087dc7eceb425ae0c401"
+    else
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/migrataur/v0.8.3/migrataur-v0.8.3-linux-amd64.tar.gz"
+      sha256 "d68b18beff3e7657b838bbb1aa7363c1de710d2fd2730b6ae9348357219eae10"
     end
   end
 
