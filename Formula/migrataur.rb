32be0b9961aaab0b7128c0ee0dcbb8571d36efb7
Saurabh Singh <saurabhsin@gmail.com>
2026-06-21 03:05:40 -0400

    migrataur v0.8.5 (devlab auto-bump)

--- Formula/migrataur.rb
+++ Formula/migrataur.rb
@@ -1,26 +1,26 @@
 class Migrataur < Formula
   desc "3-tier code migration agent"
   homepage "https://migrataur.com"
-  version "0.8.4"
+  version "0.8.5"
   license "Proprietary"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/migrataur/v0.8.4/migrataur-v0.8.4-darwin-arm64.tar.gz"
-      sha256 "9b5599edd75fd620f9cfe0e96505c8b07be6c35313e2f83c78e88e76dd0f449d"
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/migrataur/v0.8.5/migrataur-v0.8.5-darwin-arm64.tar.gz"
+      sha256 "dd7c483b316ba17acb4f014b23177571639ea80498acbf15ba0753d3f5c3a8f2"
     else
-      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/migrataur/v0.8.4/migrataur-v0.8.4-darwin-x64.tar.gz"
-      sha256 "3fb56d4f7544f8219dd454582c2635aed31a16f875f984aa13b5d2714d26dfcc"
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/migrataur/v0.8.5/migrataur-v0.8.5-darwin-x64.tar.gz"
+      sha256 "a9e33c34fdb04d0cee54b02bdfde00a9d38c6b5120407c4a3e7c34049a9028d4"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/migrataur/v0.8.4/migrataur-v0.8.4-linux-arm64.tar.gz"
-      sha256 "369b80bc8c504125451a7afcc0615127347145212a049949a6327ae5e99d4a45"
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/migrataur/v0.8.5/migrataur-v0.8.5-linux-arm64.tar.gz"
+      sha256 "3334eeb53a395e2649d1502213e5c1c42dfe73401e16e72c549c48bc41a45c8e"
     else
-      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/migrataur/v0.8.4/migrataur-v0.8.4-linux-amd64.tar.gz"
-      sha256 "67fca3b2c6aaf98a54205eb7ea8b1f87c2584ec7b8be9c9c196e5f69c535f57c"
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/migrataur/v0.8.5/migrataur-v0.8.5-linux-amd64.tar.gz"
+      sha256 "4a2bc2ca818f68a65ce72a5dbf2f80c2318fb6499855c83e74170331806298e6"
     end
   end
 
