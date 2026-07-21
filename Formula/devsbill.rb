befa3917eb22a0766c553be866ac0aa48843119a
Saurabh Singh <saurabhsin@gmail.com>
2026-07-20 20:48:29 -0400

    devsbill v0.5.7 (devlab auto-bump)

--- Formula/devsbill.rb
+++ Formula/devsbill.rb
@@ -1,26 +1,26 @@
 class Devsbill < Formula
   desc "Multi-cloud cost tracker with IaC scanning"
   homepage "https://devsbill.com"
-  version "0.5.6"
+  version "0.5.7"
   license "Proprietary"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/devsbill/v0.5.6/devsbill-v0.5.6-darwin-arm64.tar.gz"
-      sha256 "1223a034182ebc1acc1124a7575244c9ccb17e4b71ebe72760ecd16149922cbf"
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/devsbill/v0.5.7/devsbill-v0.5.7-darwin-arm64.tar.gz"
+      sha256 "296134adb5f122aab325775b9dc89c3889bbd946cf9486cf9d753b6b8af1e8fc"
     else
-      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/devsbill/v0.5.6/devsbill-v0.5.6-darwin-x64.tar.gz"
-      sha256 "734dcd50b62e00f40b5274ec2bae81bc611ae7bb9c27b3932fb7d73ce3e4440f"
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/devsbill/v0.5.7/devsbill-v0.5.7-darwin-x64.tar.gz"
+      sha256 "5e996a0f9a53affc4a87d3d8e81822329905c7bdf08c539b14e27ebc59e2711c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/devsbill/v0.5.6/devsbill-v0.5.6-linux-arm64.tar.gz"
-      sha256 "6025aa894ae06c01b45d1358a62e54b0f11efee5b6d3280424d9ad79cfc3a2e0"
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/devsbill/v0.5.7/devsbill-v0.5.7-linux-arm64.tar.gz"
+      sha256 "7143fbb13645c275921d3076224728dea7af40021b9e6f912507581d30c34141"
     else
-      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/devsbill/v0.5.6/devsbill-v0.5.6-linux-amd64.tar.gz"
-      sha256 "68028c379d4d65012541126cb46ab0e7a60d3a229b3a16e62dd3baa5a6779b50"
+      url "https://pub-6960894e747c4eacaf9e78494e0c9286.r2.dev/devsbill/v0.5.7/devsbill-v0.5.7-linux-amd64.tar.gz"
+      sha256 "5466cc673773028e1c84462650bc9651d76f190f4800ca60957b139821a3a494"
     end
   end
 
